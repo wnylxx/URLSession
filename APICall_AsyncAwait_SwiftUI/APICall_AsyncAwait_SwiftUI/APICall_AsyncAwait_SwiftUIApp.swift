7e105e99d4e2d9e8d58ce5dab5c5fe0b2474e679
WonyeolHEO <hyy96hyy@naver.com>
2024-09-25 23:35:20 +0900

    GitHub API 연결

---
+++ APICall_AsyncAwait_SwiftUI/APICall_AsyncAwait_SwiftUI/APICall_AsyncAwait_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICall_AsyncAwait_SwiftUIApp.swift
+//  APICall_AsyncAwait_SwiftUI
+//
+//  Created by wonyoul heo on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct APICall_AsyncAwait_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APICall_AsyncAwait_SwiftUI/APICall_AsyncAwait_SwiftUI/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  APICall_AsyncAwait_SwiftUI
+//
+//  Created by wonyoul heo on 9/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var user: GitHubUser?
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            
+            AsyncImage(url: URL(string: user?.avatarUrl ?? "")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+            } placeholder: {
+                Circle()
+                    .foregroundStyle(.secondary)
+            }
+            .frame(width: 120, height: 120)
+            
+            Text(user?.login ?? "No Login")
+                .bold()
+                .font(.title3)
+            
+            Text(user?.bio ?? "Please enter the valid ID")
+                .padding()
+            Spacer()
+        }
+        .padding()
+        .task {
+            do {
+                user = try await getUser()
+            } catch GitHubAPIError.invalidURL {
+                print("invalid URL")
+            } catch GitHubAPIError.invalidResponse {
+                print("invalid Response")
+            } catch GitHubAPIError.invalidData {
+                print("invalid Data")
+            } catch {
+                print("unexpected error")
+            }
+            
+        }
+    }
+    
+    
+    func getUser() async throws -> GitHubUser {
+        let endpoint = "https://api.github.com/users/wnylxx"
+        
+        guard let url = URL(string: endpoint) else {
+            throw GitHubAPIError.invalidURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw GitHubAPIError.invalidResponse
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            // avatarUrl의 경우 camel case 로 우리가 설정했는데 실제로 JSON으로 받는 데이터는 snake case 이다.
+            return try decoder.decode(GitHubUser.self, from: data)
+        } catch {
+            throw GitHubAPIError.invalidData
+        }
+    }
+    
+}
+
+
+
+struct GitHubUser: Codable {
+    // 데이터를 받아오는 작업만 할 것이라 Decodable 써도 상관 없음
+    let login: String
+    let avatarUrl: String
+    let bio: String
+}
+
+enum GitHubAPIError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+}
+
+
+
+#Preview {
+    ContentView()
+}
